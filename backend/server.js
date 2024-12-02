a1bd05ec999531ae8d7320c604be4a8626c63575
AndrewDKeys <148482347+AndrewDKeys@users.noreply.github.com>
2024-12-02 15:02:15 -0500

    SignUp v1 created

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,33 @@
 import App from "./src/App.js";
+import User from "./src/schemas/account/User.js";
+
+//User SignUp
+App.post('/createUser', async(req, res) => {
+    console.log(`SignUp Attempt Recieved: ${req.body.email}`);
+    try {
+        const { username, email, password } = req.body;
+        User.exists({username: username}).then(async(result) => {
+            if(Object.is(result, null)) { //no user exists
+                const date_created = new Date(); //get date when recipe was created
+                const status = "unverified"; //users account status
+                const measurement = "imperial";
+
+                const newUser = new User({username, email, password_hash: password, date_created, status, measurement});
+                await newUser.save()
+
+                console.log(`New User Created: ${newUser}`)
+            } else {
+                console.log(`Username: ${username} already exists`);
+                res.status(400).send({message: "User already exists"});
+            }
+        });
+
+    } catch(err) {
+        console.log(`Error during signup: ${err}`);
+        res.status(500).send(err);
+    }
+});
+
+App.post('/getUser', async(req, res) => {
+
+});
--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 const SignUp = () => {
@@ -7,14 +7,18 @@ const SignUp = () => {
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
 
-    const navigate = useNavigate();
-
     const handleSubmit = async(event, username, password, email) => {
         event.preventDefault();
 
-        navigate('/Home'); //temporary until we can get user signup up and running
-
         //axios post for user signup and verification
+        try {
+            axios.post('http://localhost:9000/createUser', { username, password, email })
+                .catch((err) => {
+                    alert("Error in signing up");
+                });
+        } catch(err) {
+            console.log(err);
+        }
     }
 
     return (
