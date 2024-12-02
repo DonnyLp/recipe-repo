0ee55faca3238125ce55ee37d43a27c2329e5f84
AndrewDKeys <148482347+AndrewDKeys@users.noreply.github.com>
2024-12-02 00:46:45 -0500

    Beginning Navbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Route, createBrowserRouter, RouterProvider, createRoutesFromElements } from 'react-router-dom';
