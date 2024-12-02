13f05fb34caf9dd2a1c9b086cf661068d7f3fc55
Edersen Cadeau <edersenc@gmail.com>
2024-12-02 02:48:01 -0500

    Added DB_Url. and Fixed connection not posting.

--- backend/src/App.js
+++ backend/src/App.js
@@ -3,7 +3,8 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 
 const port = 9000;
-const database_url = 'X';
+const database_url = 'mongodb+srv://icsi418:418repo@reciperepo.1iolb.mongodb.net/users';
+
 
 const App = express();
 
@@ -12,9 +13,16 @@ App.use(cors());
 App.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 });
-mongoose.connect(database_url)
-  .then(r => r.connection.on(
-    'error', (error) => console.log(error)))
-  .then(r => r.once('connected', () => console.log('Databased Connected')))
+
+mongoose.connect(database_url).catch(err => console.log(err));
+const dbConnection = mongoose.connection;
+dbConnection.on('error', (err) => {
+  console.log('connection error', err);
+})
+dbConnection.once('open', () => {
+  console.log('connected to database');
+})
+
+
 
 export default App
