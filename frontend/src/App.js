2fb8f141b73e776df408bb64623dcaf3285761e9
Dondario <dondariolapointe8@gmail.com>
2024-12-02 15:49:01 -0500

    - implement get and post recipes endpoints

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 //Page Imports
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
 import Profile from './pages/Profile';
 import CommunityGuidelines from './pages/CommunityGuidelines';
 import PostRecipeForm from './pages/PostRecipeForm';
